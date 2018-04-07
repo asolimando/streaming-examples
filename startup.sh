2f819013e5c723dae55a7e8a0caa5f4275808032
Alessandro Solimando <18898964+asolimando@users.noreply.github.com>
2018-04-07 09:37:13 +0200

    gitignore added, updated kafka local path

--- startup.sh
+++ startup.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-./kafka_2.11-1.0.0/bin/zookeeper-server-start.sh ./kafka_2.11-1.0.0/config/zookeeper.properties > zookeeper.log&
+
+kafkadir="kafka_2.11-1.1.0"
+
+~/apps/$kafkadir/bin/zookeeper-server-start.sh ~/apps/$kafkadir//config/zookeeper.properties > zookeeper.log&
 sleep 10
-./kafka_2.11-1.0.0/bin/kafka-server-start.sh ./kafka_2.11-1.0.0/config/server.properties > kafka.log&
+~/apps/$kafkadir/bin/kafka-server-start.sh ~/apps/$kafkadir/config/server.properties > kafka.log&
 sleep 10
-./kafka_2.11-1.0.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic origin
-./kafka_2.11-1.0.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic flink-destination
-./kafka_2.11-1.0.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic kafka-destination
-./kafka_2.11-1.0.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic spark-destination
+~/apps/$kafkadir/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic origin
+~/apps/$kafkadir/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic flink-destination
+~/apps/$kafkadir/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic kafka-destination
+~/apps/$kafkadir/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic spark-destination
 
 #./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic flink-destination --from-beginning --property print.key=true --property key.separator=:
 #./bin/kafka-console-producer.sh --broker-list localhost:9092 --topic origin --property parse.key=true --property key.separator=:
-
-
-
-
-
